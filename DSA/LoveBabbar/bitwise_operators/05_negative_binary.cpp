31a7e3bfa83888131abf8342c901163593d110ca
shubhamg <shubhamg200204@gmail.com>
2024-07-20 23:40:36 +0530

    a

--- DSA/LoveBabbar/bitwise_operators/05_negative_binary.cpp
+++ DSA/LoveBabbar/bitwise_operators/05_negative_binary.cpp
@@ -1,23 +1,23 @@
-//binary of negative numbers
+// //binary of negative numbers
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int neg_binary(int n){
-    n = ~n;
-    n = n+1;
+// int neg_binary(int n){
+//     n = ~n;
+//     n = n+1;
 
-    int res = 0;
+//     int res = 0;
 
-    while(n > 0){
-        res += (res * 10) + ()
-    }
-    return n;
-}
+//     while(n > 0){
+//         res += (res * 10) + ()
+//     }
+//     return n;
+// }
 
-int main(){
-    int n;
-    cin>>n;
+// int main(){
+//     int n;
+//     cin>>n;
 
-    cout<<neg_binary(n);
-}
+//     cout<<neg_binary(n);
+// }
