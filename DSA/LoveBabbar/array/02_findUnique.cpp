9f301f34278ad08f225519466b33013533bd8335
shubhamg <shubhamg200204@gmail.com>
2024-07-22 19:05:38 +0530

    a

--- DSA/LoveBabbar/array/02_findUnique.cpp
+++ DSA/LoveBabbar/array/02_findUnique.cpp
@@ -5,6 +5,19 @@
 #include<iostream>
 using namespace std;
 
+int findUnique(int a[], int n){
+    int ans = 0;
+
+    for(int i = 0; i < n; i++){
+        ans = ans ^ a[i];
+    }
+
+    return ans;
+}
+
 int main(){
-    
+    int a[] = { 2, 3, 1, 6, 3, 6, 2 };
+    int n = 7;
+
+    cout<<"Unique is : "<<findUnique(a, n);
 }
