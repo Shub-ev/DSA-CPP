06944f5f904a42844f2fbe8431583b3833048e96
shubhamg <shubhamg200204@gmail.com>
2024-07-22 09:13:34 +0530

    a

--- DSA/LoveBabbar/array/01_swapAlternet.cpp
+++ DSA/LoveBabbar/array/01_swapAlternet.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int mani(){
-    
+void reverseArr(int a[], int n){
+    for(int i = 0, k = n-1; i < n/2, k > n/2; i++, k--){
+        a[i] = a[i] + a[k];
+        a[k] = a[i] - a[k];
+        a[i] = a[i] - a[k];
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter Array Size: ";
+    cin>>n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    reverseArr(arr, n);
+
+    cout<<"Array after reverse:"<<endl;
+
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
 }
