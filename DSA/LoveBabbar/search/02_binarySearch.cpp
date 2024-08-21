bf8709dd4b8e05c5db2921716d96b6ce7448775a
shubhamg <shubhamg200204@gmail.com>
2024-08-21 19:20:12 +0530

    a

--- DSA/LoveBabbar/search/02_binarySearch.cpp
+++ DSA/LoveBabbar/search/02_binarySearch.cpp
@@ -27,7 +27,6 @@ void binSearch(vector<int> arr, int key){
     }
 }
 
-
 int main(){
     int n;
     cout << "Enter Array Size: ";
--- try.cpp
+++ try.cpp
@@ -1,11 +1,29 @@
 #include<iostream>
-#include<unordered_set>
 using namespace std;
 
-int main(){
-    unordered_set<int> res(10);
-    res.insert(10);
+int BinarySearch(int arr[], int k){
+    int s = 0;
+    int e = 8;
+    int m = s + (e-s)/2;
 
-    int end = res.end();
-    cout<<end<<endl;
+    while(s <= e){
+        if(arr[m] == k){
+            return m;
+        }
+        else if(arr[m] < k){
+            s = m + 1;
+        }
+        else{
+            e = m - 1;
+        }
+        m = s + (e - s)/2;
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+
+    int res = BinarySearch(arr, 2);
+    cout<<res;
 }
