4802e4d29efb841d43c281eef21b2c059c45e4e7
shubhamg <shubhamg200204@gmail.com>
2024-10-24 14:35:36 +0530

    a

---
+++ DSA/LoveBabbar/CharString/02_string_char_methods.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+// string methods
+
+int main(){
+    char arr[20];
+    cin.getline(arr, 20);       // this method accepts string with      ' '  &  '\t'  &  '\n'
+
+    string str1 = "this is a string";
+    string str2;
+
+    // strcpy(str2, str1);
+
+    // const res = strcmp(str1, str2);
+
+    return 0;
+}
