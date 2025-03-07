3d8ce7ce19d58c3d943ae72d5487deea7468783f
Shub-ev <shubhamg200204@gmail.com>
2025-03-07 16:27:53 +0530

    a

---
+++ Programming_Language_C++/Chapter1/03_print_something.cpp
@@ -0,0 +1,10 @@
+// to print something over console
+
+// instructs compiler to include declarations of the standart stream "I/O" facilities as found in iosteam
+#include<iostream>
+
+int main(){
+    // The operator << (‘‘put to’’ or ‘‘Intertion operator’’) writes its second argument onto its first
+    // the string literal "Hello, World!\n" is written onto the standard output stream std::cout
+    std::cout<<"Hello World!";
+}
