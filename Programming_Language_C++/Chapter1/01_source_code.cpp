1a0f211bcf386b767ef074a739a0647053f098e9
Shub-ev <shubhamg200204@gmail.com>
2025-03-07 16:09:07 +0530

    a

---
+++ Programming_Language_C++/Chapter1/01_source_code.cpp
@@ -0,0 +1,7 @@
+// C++ is a compiled language.
+// For a program to run, its source text has to be processed by a compiler, producing object files
+// which are combined by a linker yielding an executable program
+// A C++ program typically consists of many source code files (usually simply called source files).
+
+// each C++ program must have main() or we will get error:
+    // undefined reference to `WinMain@16'
---
+++ Programming_Language_C++/Chapter1/02_smallest_program.cpp
@@ -0,0 +1,6 @@
+// this is smallest (standard) program in CPP
+int main () {}
+
+
+// used standard because some compilers allow  => "main(){}" 
+// but it so not standard
