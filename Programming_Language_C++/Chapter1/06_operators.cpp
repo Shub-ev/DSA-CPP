ac76e1e56171fc76c6f972f3fc997ee35bf9f2ff
Shub-ev <shubhamg200204@gmail.com>
2025-03-08 13:27:59 +0530

    a

--- Programming_Language_C++/Chapter1/06_operators.cpp
+++ Programming_Language_C++/Chapter1/06_operators.cpp
@@ -19,6 +19,24 @@ int main(){
     cout<<(10 < 20)<<endl;      // less than
     cout<<(10 > 20)<<endl;      // greater than
 
-    // single assignment operator
+    // single assignment operator  (1 only)
     int num = 10;               // single assignment operator
+
+    // compound assignment operator (10 only)
+    num += 10;
+    num -= 10;
+    num *= 10;
+    num /= 10;
+    num %= 10;
+    num &= 1;
+    num |= 1;
+    num ^= 4;
+    num >>= 2;
+    num <<= 2;
+
+    // increment & decrement    (4 only)
+    ++num;
+    --num;
+    num++;
+    num--;
 }
